4cc647c651c4ca1c925d15678bda9b3627398428
rombzh <rom22bzh@gmail.com>
2025-12-06 14:55:34 +0100

    adding map

--- js/map.js
+++ js/map.js
@@ -1,14 +1,64 @@
-
+let map = null;
+let mapBounds = null;
+let mapOverlay = null;
 
 function createInteractiveMap() {
     updateLoading("Création de la map intéractive...", 35);
-}
 
+    map = L.map('map', {
+        crs: L.CRS.Simple,
+        minZoom: mapConfig.minZoom,
+        maxZoom: mapConfig.maxZoom,
+        zoomSnap: mapConfig.zoomSnap,
+        maxBoundsViscosity: 1.0,
+        inertia: false,  
+    });
 
-function addMakers() {
-    updateLoading("Ajout des différents markers...", 50);
+    // Bornes de l'image
+    mapBounds = [[0, 0], [mapConfig.height, mapConfig.width]];
+    map.setMaxBounds(mapBounds);
+    map.fitBounds(mapBounds);
+
+    // Ajouter l'image par défaut
+    mapOverlay = L.imageOverlay(mapConfig.mapOverlay, mapBounds).addTo(map);
+
 }
 
 function displayInteractiveMap() {
     updateLoading("Ajout des différents markers...", 70);
+
+    const centerY = mapConfig.height / 2;
+    const centerX = mapConfig.width / 2;
+
+    map.setZoom(map.minZoom);
+
+    map.whenReady(() => {
+        map.dragging.disable(); 
+    });
+
+    map.on("zoomend", () => {
+        if (map.getZoom() <= mapConfig.minZoom) {
+            map.dragging.disable();
+            map.setView([centerY, centerX], map.getMinZoom(), { animate: true });
+        } else {
+            map.dragging.enable();
+        }
+    });
+
+}
+
+
+function addMarkers() {
+    updateLoading("Ajout des différents markers...", 50);
+}
+
+function getCursorPos() {
+    const xCoords = document.getElementById('x-coords')
+    const yCoords = document.getElementById('y-coords')
+    map.on('mousemove', function(e) {
+        const x = e.latlng.lng.toFixed(0);
+        const y = e.latlng.lat.toFixed(0);
+        xCoords.textContent = `${x}`;
+        yCoords.textContent = `${y}`;
+    });
 }
--- js/mapConfig.js
+++ js/mapConfig.js
@@ -6,7 +6,15 @@ function createDefaultMapConfig() {
     updateLoading("Création de la configuration de la map...", 5);
 
     mapConfig = {
-        "res": ""
+        "mapOverlay": "img/map_black.jpg",
+        "width": 2160,
+        "height": 3840,
+        "minZoom": -1.6,
+        "maxZoom": 0,
+        "zoomSnap": 0,
+        "iconSize": [20, 20],
+        "iconAnchor": [10, 20],
+        "popupAnchor": [0, -20]
     }
 
     updateLoading("Création de la configuration de la map terminée...", 15);
